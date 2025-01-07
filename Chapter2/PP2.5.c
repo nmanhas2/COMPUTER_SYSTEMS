8fd375956179d567d2abb8568561cf2d15b28585
nmanhas2 <manhasnubal@gmail.com>
2025-01-06 19:38:01 -0700

    added practice problem 2.5

--- Chapter2/PP2.5.c
+++ Chapter2/PP2.5.c
@@ -122,6 +122,10 @@ formats use different encoding schemes
 */
 int main()
 {
-    test_show_bytes(12345);
+    int val = 0x87654321;
+    byte_pointer valp = (byte_pointer) &val;
+    show_bytes(valp, 1); //A
+    show_bytes(valp, 2); //B
+    show_bytes(valp, 3); //C
     return 0;
 }
